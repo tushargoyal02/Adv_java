2f78757bbecee4a51ac69f15530c01101eb7b794
Tushar Goyal <tushargoyal19tj@gmail.com>
2020-04-30 22:20:17 +0530

    demo hello program

---
+++ 019 BasicsDemo.java
@@ -0,0 +1,18 @@
+class BasicsDemo {
+
+  static void print() {
+    System.out.println("\n\nInside print ...");
+    System.out.println("Hello, world!!"); // Advance cursor to beginning of next line
+    System.out.println();                 // Print empty line
+    System.out.print("Hello, world!!");   // Cursor stayed after the printed string
+    System.out.println("Hello,");
+    System.out.print(" ");                // Print a space
+    System.out.print("world!!");
+  }
+  
+  public static void main(String[] args) {	
+	   	// calling above function
+
+		print();				
+  }       
+}
